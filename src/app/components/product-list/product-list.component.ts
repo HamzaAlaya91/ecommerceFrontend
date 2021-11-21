67b84f139e6456a6f65b25beb60620bdf6aadbc8
HamzaAlaya91 <alaya.hamza1@gmail.com>
2021-11-21 19:57:18 +0100

    add category name in the top of product list

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -17,6 +17,8 @@ export class ProductListComponent implements OnInit {
   previousCategoryId: number = 1;
   searchMode: boolean = false;
 
+  currentCategoryName: string;
+
   // new properties for pagination
   thePageNumber: number = 1;
   thePageSize: number = 5;
@@ -78,10 +80,14 @@ export class ProductListComponent implements OnInit {
     if (hasCategoryId) {
       // get the "id" param string. convert string to a number using the "+" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+
+      // get the "name" param string
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name');
     }
     else {
       // not category id available ... default to category id 1
       this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
     }
 
     //
